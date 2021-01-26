f03fe89dbc0c687bd2ac9b59d9c2324ed2c99fe3
JeongMin-D <j25ng.py@gmail.com>
2021-01-26 21:46:21 +0900

    update

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
-from .views import helloAPI, randomOX,randomMul, randomTest
+from .views import helloAPI, randomOX_hard ,randomMul_hard, randomTest_hard
+from .views import randomMul_easy, randomOX_easy, randomTest_easy
+from .views import randomMul_normal, randomOX_normal, randomTest_normal
 
 urlpatterns = [
     path('hello/', helloAPI),
-    path('Mul/<int:id>/', randomMul),
-    path('OX/<int:id>/', randomOX),
-    path('Test/<int:id>/', randomTest)
+    path('Mul/<int:id>/', randomMul_easy),
+    path('Mul/<int:id>/', randomMul_normal),
+    path('Mul/<int:id>/', randomMul_hard),
+    path('OX/<int:id>/', randomOX_easy),
+    path('OX/<int:id>/', randomOX_normal),
+    path('OX/<int:id>/', randomOX_hard),
+    path('Test/<int:id>/', randomTest_easy),
+    path('Test/<int:id>/', randomTest_normal),
+    path('Test/<int:id>/', randomTest_hard)
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -10,22 +10,64 @@ def helloAPI(request):
     return Response('hello world!')
 
 @api_view(['GET'])
-def randomMul(request, id):
-    totalMuls = Multiple.objects.all()
-    randomMuls = random.sample(list(totalMuls), id)
-    serializer = QuizSerializer(randomMuls, many=True)
+def randomMul_easy(request, id):
+    totalMuls_easy = Multiple.objects.all()
+    randomMuls_easy = random.sample(list(totalMuls_easy), id)
+    serializer = QuizSerializer(randomMuls_easy, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def randomOX(request, id):
-    totalOXs = OX.objects.all()
-    randomOXs = random.sample(list(totalOXs), id)
-    serializer = QuizSerializer(randomOXs, many=True)
+def randomMul_normal(request, id):
+    totalMuls_normal = Multiple.objects.all()
+    randomMuls_normal = random.sample(list(totalMuls_normal), id)
+    serializer = QuizSerializer(randomMuls_normal, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def randomTest(request, id):
-    totalTests = Test.objects.all()
-    randomTests = random.sample(list(totalTests), id)
-    serializer = QuizSerializer(randomTests, many=True)
+def randomMul_hard(request, id):
+    totalMuls_hard = Multiple.objects.all()
+    randomMuls_hard = random.sample(list(totalMuls_hard), id)
+    serializer = QuizSerializer(randomMuls_hard, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def randomOX_easy(request, id):
+    totalOXs_easy = OX.objects.all()
+    randomOXs_easy = random.sample(list(totalOXs_easy), id)
+    serializer = QuizSerializer(randomOXs_easy, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def randomOX_normal(request, id):
+    totalOXs_normal = OX.objects.all()
+    randomOXs_normal = random.sample(list(totalOXs_normal), id)
+    serializer = QuizSerializer(randomOXs_normal, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def randomOX_hard(request, id):
+    totalOXs_hard = OX.objects.all()
+    randomOXs_hard = random.sample(list(totalOXs_hard), id)
+    serializer = QuizSerializer(randomOXs_hard, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def randomTest_easy(request, id):
+    totalTests_easy = Test.objects.all()
+    randomTests_easy = random.sample(list(totalTests_easy), id)
+    serializer = QuizSerializer(randomTests_easy, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def randomTest_normal(request, id):
+    totalTests_normal = Test.objects.all()
+    randomTests_normal = random.sample(list(totalTests_normal), id)
+    serializer = QuizSerializer(randomTests_normal, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def randomTest_hard(request, id):
+    totalTests_hard = Test.objects.all()
+    randomTests_hard = random.sample(list(totalTests_hard), id)
+    serializer = QuizSerializer(randomTests_hard, many=True)
     return Response(serializer.data)
