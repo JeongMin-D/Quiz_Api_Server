93a7cce6baf4222402dd67f936c0983256df2e96
JeongMin-D <j25ng.py@gmail.com>
2021-01-26 22:10:27 +0900

    update3

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
-from .models import Multiple, OX
+from .models import Multiple_easy, OX_easy, Test_easy
+from .models import Multiple_normal, OX_normal, Test_normal
+from .models import Multiple_hard, OX_hard, Test_hard
+
 
 class QuizSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Multiple
+        model = Multiple_easy
         fields = ('title', 'body', 'answer')
